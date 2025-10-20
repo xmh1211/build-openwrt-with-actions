96dd7d7ec4040ea946225ab83a4a2e95e11a9e56
xmh1211 <157801666+xmh1211@users.noreply.github.com>
2025-10-20 14:26:49 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,8 +1,12 @@
 # 请在下方输入自定义命令(一般用来安装第三方插件)(可以留空)
 sed -i "s/$192.168.1.1/$192.168.100.1/g" package/base-files/files/bin/config_generate
+
 sed -i "s/$hostname='ImmortalWrt'/$hostname='X-WRT'/g" package/base-files/files/bin/config_generate
+
 sed -i "s/$timezone='UTC'/$timezone='CST-8'/g" package/base-files/files/bin/config_generate
+
 sed -i '/CST-8/a \\t\tset system.@system[-1].zonename='Asia/Shanghai'' package/base-files/files/bin/config_generate
+
 cat > feeds.conf.default <<'EOF'
 src-git packages https://github.com/immortalwrt/packages.git^9bff73b6942b00e3f6b42e9261c9db7bdfcd08a2
 src-git luci https://github.com/immortalwrt/luci.git^5ff7e613f8ac26c177712ab6df98701d160a29d4
