965889520730f1ffe8101ebd5c23126526504918
xmh1211 <157801666+xmh1211@users.noreply.github.com>
2025-10-20 14:26:21 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,20 +1,12 @@
 # 请在下方输入自定义命令(一般用来安装第三方插件)(可以留空)
-
-# 编辑默认的lan口ip地址
-sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
-
-# 编辑默认的主题
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# 编辑默认的luci显示的固件名称
-#sed -i 's/OpenWrt/ZWRT/g' package/base-files/files/bin/config_generate
-#sed -i 's/ImmortalWrt/ZWRT/g' package/base-files/files/bin/config_generate
-
-# 添加额外的软件包，echo 方式和git clone 方式二选一即可
-#echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git UA3F https://github.com/SunBK201/UA3F.git' >>feeds.conf.default
-#git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
-#git clone https://github.com/kenzok8/small.git package/small
-#git clone https://github.com/SunBK201/UA3F.git package/UA3F
-git clone https://github.com/stevenjoezhang/luci-app-adguardhome.git package/ADGH
+sed -i "s/$192.168.1.1/$192.168.100.1/g" package/base-files/files/bin/config_generate
+sed -i "s/$hostname='ImmortalWrt'/$hostname='X-WRT'/g" package/base-files/files/bin/config_generate
+sed -i "s/$timezone='UTC'/$timezone='CST-8'/g" package/base-files/files/bin/config_generate
+sed -i '/CST-8/a \\t\tset system.@system[-1].zonename='Asia/Shanghai'' package/base-files/files/bin/config_generate
+cat > feeds.conf.default <<'EOF'
+src-git packages https://github.com/immortalwrt/packages.git^9bff73b6942b00e3f6b42e9261c9db7bdfcd08a2
+src-git luci https://github.com/immortalwrt/luci.git^5ff7e613f8ac26c177712ab6df98701d160a29d4
+src-git routing https://github.com/openwrt/routing.git^885ae6f9d0c0f47a1a6321e1cc7e9c7e20cc2681
+src-git telephony https://github.com/openwrt/telephony.git^2a4541d46199ac96fac214d02c908402831c4dc6
+src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main
+EOF
